1769fcb28a5882aa2dc4e4c9db75413e12a00747
Filipe Dias <filipe77dias@gmail.com>
2024-02-27 15:08:11 +0000

    Fix settings to use value on configs.py

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from main.configs import DJANGO_SECRET
 import mimetypes
 import os
 import pytz
@@ -32,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", DJANGO_SECRET)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
