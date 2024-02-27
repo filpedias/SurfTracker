0cd21a390d2b940fddf9829ec5a592ebe0932624
Filipe Dias <filipe77dias@gmail.com>
2024-02-27 15:08:25 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,16 +7,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-    from django.conf import settings
-
-    #* This debug if statement runs a debugger that is attachable by Visual Studio Code, comment if not necessary
-    if settings.DEBUG:
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-            import debugpy
-            debugpy.listen(("0.0.0.0", 3001))
-            print('Attached!')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
